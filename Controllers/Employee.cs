3fda5ef8fd347f5cce628d3d7dc3985455c8aa29
A-RAMAKRISHNANS1 <A-RAMAKRISHNANS1@VZTCHI-2C9D3BCE>
2022-02-05 18:50:57 -0600

    Added sql connection string

---
+++ Controllers/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace testdashboard02032022.Controllers
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
